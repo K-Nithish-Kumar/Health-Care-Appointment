9600c632e868900b4834029bc6c7d8506eea781e
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-01 12:34:59 +0530

    Adding payment method using RazorPay

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -6,10 +6,16 @@ import { toast } from 'react-toastify';
 import { useEffect } from 'react';
 
 const MyAppointments = () => {
-  const {backendUrl, token} = useContext(AppContext);
+  const {backendUrl, token, getDoctorsData} = useContext(AppContext);
 
 
   const [appointments,setAppointments] = useState([])
+  const months = [" ","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split('_')
+    return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+  }
 
   const getUserAppointments = async () => {
     try {
@@ -25,6 +31,56 @@ const MyAppointments = () => {
     } catch (error) {
       console.log(error);
       toast.error(error.message)
+    }
+  }
+
+  const cancelAppointment = async(appointmentId) => {
+    try {
+
+      const {data} = await axios.post(backendUrl + '/api/user/cancel-appointment',{appointmentId},{headers:{token}})
+      if (data.success) {
+        toast.success(data.message)
+        getUserAppointments()
+        getDoctorsData();
+      } else {
+        toast.error(data.message)
+      }
+      
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
+  const initPay = (order) => {
+    const options = {
+      key:import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount:order.amount,
+      currency:order.currency,
+      name:"Appointment Payment",
+      description:"Appointment Payment",
+      order_id:order.id,
+      receipt:order.receipt,
+      handler:async (response) => {
+        console.log(response);
+        
+      }
+    }
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+  };
+
+  const appointmentRazorpay = async (appointmentId) => {
+    try {
+      
+      const {data} = await axios.post(backendUrl + "/api/user/payment-razorpay",{appointmentId},{headers:{token}})
+      if (data.success) {
+        initPay(data.order)
+      }
+
+
+    } catch (error) {
+      
     }
   }
 
@@ -51,14 +107,15 @@ const MyAppointments = () => {
                 <p className='text-zinc-700 font-medium mt-1'>Address:</p>
                 <p className='text-xs'>{item.docData.address.line1}</p>
                 <p className='text-xs'>{item.docData.address.line2}</p>
-                <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span>{item.slotDate} | {item.slotTime}</p>
+                <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span>{slotDateFormat(item.slotDate)} | {item.slotTime}</p>
               </div>
               <div>
 
               </div>
               <div className='flex flex-col gap-2 justify-end'>
-                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
-                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>
+                {!item.cancelled && <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+                {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
+                {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment Cancelled</button>}
               </div>
             </div>
           ))}
